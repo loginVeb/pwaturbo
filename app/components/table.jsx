3edfd677c2349a30dcf344ba8afa562a09113e50
loginVeb <loginVeb@yandex.ru>
2024-12-21 03:46:21 +0300

    раб список без динамики

--- app/components/table.jsx
+++ app/components/table.jsx
@@ -1,15 +1,22 @@
 'use client'
 
+import { useState } from 'react';
+
 const guardNames = ['Логинов', 'Захаров', 'Орлов', 'Цветков', 'Тихомиров'];
 
 function Table({ styles }) {
+  const [selectedGuards, setSelectedGuards] = useState([]);
+
+  const handleAddGuard = (newGuard) => {
+    setSelectedGuards(prevGuards => [...prevGuards, newGuard]);
+  };
+
   return (
     <div className={styles.table}>
-     
-
-      <select className={styles.field}>
-        {guardNames.map((guardName) => (
-          <option key={guardName} value={guardName}>
+      <select className={styles.field} onChange={(e) => handleAddGuard(e.target.value)}>
+        <option value="">Выберите охранника</option>
+        {guardNames.map((guardName, index) => (
+          <option key={guardName} value={guardName} data-index={index + 1}>
             {guardName}
           </option>
         ))}
@@ -19,20 +26,17 @@ function Table({ styles }) {
         Добавить охранника
       </button>
 
-      <button className={styles.confirmButton}>
+      <button className={styles.confirmButton} onClick={() => console.log(selectedGuards)}>
         Подтвердить список
       </button>
-
-      <ul className={styles.list}>
-        <li className={styles.listItem}>
-          
-        </li>
-      </ul>
+      <div className={styles.dvList}>
+        <ul className={styles.list}>
+          {selectedGuards.map((guardName, index) => (
+            <li key={index} className={styles.listItem} data-index={index + 1}>{guardName}</li>
+          ))}
+        </ul>
+      </div>
     </div>
-    
-      
-
-
   );
 }
 
